9d1b254b9c5248a2c4bf2f18b54f5b3c99889490
Paulo Jansen <paulo.jansen@vespergaming.com>
2016-07-28 13:23:14 -0300

    update house pricings

--- config/buildings/house.rb
+++ config/buildings/house.rb
@@ -6,20 +6,19 @@
         {
         },
         {
-            wood: 100,
-            food: 25,
+            wood: 90,
             time: 8,
-            population: 1
+            population: 2
         },
         {
-            wood: 150,
-            gold: 50,
+            wood: 180,
+            stone: 50,
             time: 44,
             population: 1
         },
         {
-            wood: 320,
-            gold: 90,
+            wood: 420,
+            gold: 200,
             time: 88,
             population: 1
         }
