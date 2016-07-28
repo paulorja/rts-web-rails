092cad73378302c0b84ea7ae86d501f233709228
Paulo Jansen <paulo.jansen@vespergaming.com>
2016-07-28 17:39:27 -0300

    remove to_grass costs

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,14 +1,11 @@
 TO_GRASS = {
     tree: {
-        wood: 150,
         time: 50
     },
     gold: {
-        gold: 150,
         time: 50
     },
     stone: {
-        stone: 150,
         time: 50
     }
 }
