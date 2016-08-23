40071b23d2403e0a791e3e37f204b0922a3968c4
Paulo Jansen <pauloo.jansen@gmail.com>
2016-08-23 17:29:08 +0000

    castelos dão 15 territórios ao inves de 10

--- config/buildings/castle.rb
+++ config/buildings/castle.rb
@@ -7,21 +7,21 @@
         {
         },
         {
-            territories: 10,
+            territories: 15,
             score: 1
         },
         {
             stone: 120,
             gold: 15,
             time: 15,
-            territories: 10,
+            territories: 15,
             score: 1
         },
         {
             stone: 280,
             gold: 35,
             time: 150,
-            territories: 10,
+            territories: 15,
             score: 2
         },
         {
@@ -35,21 +35,21 @@
             stone: 900,
             gold: 180,
             time: 750,
-            territories: 10,
+            territories: 15,
             score: 2
         },
         {
             stone: 1500,
             gold: 780,
             time: 1350,
-            territories: 10,
+            territories: 15,
             score: 2
         },
         {
             stone: 3250,
             gold: 1490,
             time: 2400,
-            territories: 10,
+            territories: 15,
             score: 2
         },
         {
@@ -84,35 +84,35 @@
             stone: 52500,
             gold: 30500,
             time: 33100,
-            territories: 10,
+            territories: 15,
             score: 3
         },
         {
             stone: 85500,
             gold: 44500,
             time: 43100,
-            territories: 10,
+            territories: 15,
             score: 3
         },
         {
             stone: 105555,
             gold: 74500,
             time: 53100,
-            territories: 10,
+            territories: 15,
             score: 4
         },
         {
             stone: 125500,
             gold: 104500,
             time: 63100,
-            territories: 10,
+            territories: 15,
             score: 4
         },
         {
             stone: 145000,
             gold: 120250,
             time: 83100,
-            territories: 10,
+            territories: 15,
             score: 4
         }
     ]
