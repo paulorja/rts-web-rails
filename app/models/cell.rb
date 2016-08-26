680f508d79517cdcaa4f1df99553c7c1373fb6fb
Paulo Jansen <paulo.jansen@vespergaming.com>
2016-08-26 17:07:52 -0300

    fix can build wall

--- app/models/cell.rb
+++ app/models/cell.rb
@@ -196,7 +196,7 @@ class Cell < ActiveRecord::Base
 
     (0..8).each do |i|
       if i != 4
-        return true if arredores[i].user_id == user_id and !arredores[i].is_wall
+        return true if arredores[i].user_id == user_id and !arredores[i].is_wall and arredores[i].is_grass
       end
     end
 
