1d40ff503664b7dd0ebe33220e0692f71e6c6c8a
PAULO JANSEN <pauloo.jansen@gmail.com>
2016-08-27 12:43:23 -0300

    add random color

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,6 +1,8 @@
 class VisitorsController < ApplicationController
 
   def home
+    @random_color = '#' + ("%06x" % (rand * 0xffffff)).to_s
+
     render file: 'visitors/home', layout: false
   end
 
