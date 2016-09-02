f3c4860dd5f9052f18ce556532a65a20bc229a50
Paulo Jansen <paulo.jansen@vespergaming.com>
2016-09-02 14:33:37 -0300

    add pop ranking

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -8,4 +8,8 @@ class RankingController < ApplicationController
     @users = User.includes(:user_data).joins(:user_data).where('user_type = 0').order('user_data.total_territories DESC')
   end
 
+  def territories
+    @users = User.includes(:user_data).joins(:user_data).where('user_type = 0').order('user_data.total_pop DESC')
+  end
+
 end
