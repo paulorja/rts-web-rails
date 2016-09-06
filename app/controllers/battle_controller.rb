b92db673c23d915d753a33e5363457f4c3e1bdbf
Paulo Jansen <paulo.jansen@vespergaming.com>
2016-09-06 09:48:41 -0300

    batalha em construcao

--- app/controllers/battle_controller.rb
+++ app/controllers/battle_controller.rb
@@ -2,10 +2,18 @@ class BattleController < ApplicationController
 
 
   def attack
+    funcao_em_construcao
+    redirect_to :back
+    return
+
     @armies = @current_user.all_armies
   end
 
   def send_attack
+    funcao_em_construcao
+    redirect_to :back
+    return
+    
     start_battle = EventBattle.start_event(@current_user, params[:cell_id], params[:unit], params[:total_steps])
 
     if start_battle.is_a? String
@@ -15,4 +23,11 @@ class BattleController < ApplicationController
       render json: start_battle.battle_data
     end
   end
+
+  private
+    def funcao_em_construcao
+      flash[:alert] = 'As batalhas ainda não estão disponíveis.'
+      return
+    end
+
 end
